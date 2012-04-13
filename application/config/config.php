bbd4c811f7f297693b603630fff6ac9b0822fa61
Lonoto <lonoto.org@gmail.com>
2012-04-13 22:44:38 +0200

    Improvements in the layout

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,8 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+//$config['index_page'] = 'index.php';  //
+$config['index_page'] = 'index.php';  
 
 /*
 |--------------------------------------------------------------------------
@@ -224,7 +225,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '1wW_e3@f.e#dw2qE34ff3?#?#WEPQW23w_';
 
 /*
 |--------------------------------------------------------------------------
@@ -279,7 +280,7 @@ $config['cookie_secure']	= FALSE;
 | COOKIE data is encountered
 |
 */
-$config['global_xss_filtering'] = FALSE;
+$config['global_xss_filtering'] = FALSE; //FIXME
 
 /*
 |--------------------------------------------------------------------------
@@ -293,7 +294,7 @@ $config['global_xss_filtering'] = FALSE;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = FALSE;
+$config['csrf_protection'] = FALSE;  //FIXME
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
